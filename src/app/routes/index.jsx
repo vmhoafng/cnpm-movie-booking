aa940fde62fb24b04de463da9b000d426653e1d1
vmhoafng <vominhhoang044@gmail.com>
2023-09-21 18:58:46 +0700

    merge setup

--- src/app/routes/index.jsx
+++ src/app/routes/index.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import { useRoutes } from 'react-router-dom';
+import React from "react";
+import { useRoutes } from "react-router-dom";
 
 //const Component = React.lazy(()=>import(''));
 
 function AllRoutes() {
-	return useRoutes([
-		{
-			path: '/',
-			element: '',
-			children: [],
-		},
-	]);
+  return useRoutes([
+    {
+      path: "/",
+      element: "",
+      children: [],
+    },
+  ]);
 }
 
 export default AllRoutes;
