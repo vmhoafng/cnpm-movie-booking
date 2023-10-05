aa874d877fc0c0ad04cc829f0001b8510ae06b74
vmhoafng <vominhhoang044@gmail.com>
2023-10-05 14:30:22 +0700

    update components

---
+++ src/pages/client/cinema/Cinema.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Cinema() {
+  return (
+    <div>Cinema</div>
+  )
+}
+
+export default Cinema
---
+++ src/pages/client/seatPlan/SeatPlan.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SeatPlan() {
+  return (
+    <div>SeatPlan</div>
+  )
+}
+
+export default SeatPlan
